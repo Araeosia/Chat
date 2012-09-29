385e42006045c45a65a98f26d414112aa2edcc2b
AgentKid <agentkid20@gmail.com>
2012-09-29 16:51:56 -0300

    Fixed tabs, fixed overrides

--- src/me/zippy120/CrossServerChat.java
+++ src/me/zippy120/CrossServerChat.java
@@ -13,110 +13,49 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CrossServerChat extends JavaPlugin{
-<<<<<<< HEAD
-=======
 	
-	Logger log = Logger.getLogger("Minecraft");
-	static CrossServerChat plugin;
-	IRCBot bot = new IRCBot();
-	Map<Player, Boolean> recieve = new HashMap<Player, Boolean>();
-	
-	
-	public void onEnable(){
-		log.info("Your plugin has been enabled!");
-		log.info("Enabling IRC bot...");
->>>>>>> bae7ef2... Revert "Nefiooefwewf"
-
     static final Logger log = Logger.getLogger("Minecraft");
     static CrossServerChat plugin;
     IRCBot bot = new IRCBot(this);
-    Map<String, Boolean> recieve = new HashMap<String, Boolean>();
-    
-    
-    
-    //STOOOOOOOOOOOOFFFFFFFFFFFF
-    public String 
-    
-    
-    $username = "AraeosiaServers";
-    
-    
-    public String
-    
-    
-    $server = "irc.esper.net";
-    
-    
-    public ArrayList<String>
-    
-    
-    $channels = new ArrayList<String>();
-    
-    private void stuff(){
-    	$channels.add("#araeosia-servers");
-    }
-    
-    //END :D
+    Map<Player, Boolean> recieve = new HashMap<Player, Boolean>();
 
     @Override
     public void onEnable(){
-    	stuff();
         log.info("Your plugin has been enabled!");
         log.info("Enabling IRC bot...");
-        
+
         // Connect to the IRC server.
         try {
-            bot.startBot();
+           bot.startBot();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Join the channel.
+        bot.joinChannel("#araeosia-servers");
 	
     }
 
-	public void onDisable(){
-		log.info("Your plugin has been disabled.");
-		log.info("Disabling IRC bot...");
-		bot.disconnect();
-	}
-	public void onPlayerChatEvent(AsyncPlayerChatEvent event){
-		String message = event.getFormat() + event.getMessage();
-		bot.sendMessage("#araeosia-servers", message);
-	}
+    @Override
+    public void onDisable(){
+        log.info("Your plugin has been disabled.");
+        log.info("Disabling IRC bot...");
+        bot.disconnect();
+    }
+    public void onPlayerChatEvent(AsyncPlayerChatEvent event){
+        String message = event.getFormat() + event.getMessage();
+        bot.sendMessage("#araeosia-servers", message);
+    }
+    public void sendToServer(String message) {
+        for (Player p : plugin.getServer().getOnlinePlayers()){
+            if (isRecieveingMessages(p))
+                p.sendMessage(message);
+        }	
+    }
 
-
-	public void sendToServer(String message) {
-		for (Player p : plugin.getServer().getOnlinePlayers()){
-			if (isRecieveingMessages(p))
-				p.sendMessage(message);
-		}
-		
-	}
-
-	private boolean isRecieveingMessages(Player p) {
-		if(recieve.containsKey(p) && recieve.get(p) == true)
-			return true;
-		return false;
-	}
-	public boolean onCommand(CommandSender sender,  Command cmd, String commandLabel, String[] args){
-		if (cmd.getName().equalsIgnoreCase("CC")){
-			if (recieve.containsKey(sender)){
-				if (recieve.get(sender) == true){
-					sender.sendMessage(ChatColor.RED + "Cross-Server chat disabled.");
-					recieve.put(sender.getName(), false);
-					return true;
-				} else {
-					sender.sendMessage(ChatColor.YELLOW + "Cross-Server chat enabled.");
-					recieve.put(sender.getName(), true);
-					return true;
-				}
-			} else {
-				sender.sendMessage(ChatColor.YELLOW + "Cross-Server chat enabled.");
-				recieve.put(sender.getName(), true);
-				return true;
-			}
-		}
-		return false;
-		
-	}
-
+    private boolean isRecieveingMessages(Player p) {
+        if(recieve.containsKey(p) && recieve.get(p) == true)
+            return true;
+        return false;
+    }
 }
