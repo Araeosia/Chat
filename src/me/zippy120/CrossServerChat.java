6dfab9d8b3c213676153ddb696304138be6d0930
Daniel Barnes <daniel-barnes@hotmail.com>
2012-09-29 13:24:18 -0600

    Initial commit :D

---
+++ src/me/zippy120/CrossServerChat.java
@@ -0,0 +1,84 @@
+package me.zippy120;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrossServerChat extends JavaPlugin{
+	
+	Logger log = Logger.getLogger("Minecraft");
+	static CrossServerChat plugin;
+	IRCBot bot = new IRCBot();
+	Map<Player, Boolean> recieve = new HashMap<Player, Boolean>();
+	
+	
+	public void onEnable(){
+		log.info("Your plugin has been enabled!");
+		log.info("Enabling IRC bot...");
+
+        // Connect to the IRC server.
+		try {
+			IRCBot.startBot();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+        // Join the channel.
+        bot.joinChannel("#araeosia-servers");
+		
+	}
+
+	public void onDisable(){
+		log.info("Your plugin has been disabled.");
+		log.info("Disabling IRC bot...");
+		bot.disconnect();
+	}
+	public void onPlayerChatEvent(AsyncPlayerChatEvent event){
+		String message = event.getFormat() + event.getMessage();
+		bot.sendMessage("#araeosia-servers", message);
+		
+	}
+
+
+	public void sendToServer(String message) {
+		for (Player p : plugin.getServer().getOnlinePlayers()){
+			if (isRecieveingMessages(p))
+				p.sendMessage(message);
+		}
+		
+	}
+
+	private boolean isRecieveingMessages(Player p) {
+		if(recieve.containsKey(p) && recieve.get(p) == true)
+			return true;
+		return false;
+	}
+	public boolean onCommand(CommandSender sender,  Command cmd, String commandLabel, String[] args){
+		if (cmd.getName().equalsIgnoreCase("CC")){
+			if (recieve.containsKey(sender)){
+				if (recieve.get(sender) == true){
+					sender.sendMessage(ChatColor.RED + "Cross-Server chat disabled.");
+					recieve.put((Player) sender, false);
+					return true;
+				} else {
+					sender.sendMessage(ChatColor.YELLOW + "Cross-Server chat enabled.");
+					recieve.put((Player) sender, true);
+					return true;
+				}
+			} else {
+				sender.sendMessage(ChatColor.YELLOW + "Cross-Server chat enabled.");
+				recieve.put((Player) sender, true);
+				return true;
+			}
+		}
+		return false;
+		
+	}
+}
---
+++ src/me/zippy120/IRCBot.java
@@ -0,0 +1,38 @@
+package me.zippy120;
+
+import java.io.IOException;
+
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.NickAlreadyInUseException;
+import org.jibble.pircbot.PircBot;
+
+public class IRCBot extends PircBot{
+	protected CrossServerChat plugin;
+	public IRCBot(){
+		this.setName("DarkHelmetMainBot");
+	}
+	
+    public void onMessage(String channel, String sender, String login, String hostname, String message){
+    	plugin.sendToServer(message);
+    }
+
+	public static void startBot(){
+		
+		IRCBot bot = new IRCBot();
+		
+        // Connect to the IRC server.
+			try {
+				bot.connect("irc.esper.net");
+			} catch (NickAlreadyInUseException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (IrcException e) {
+				e.printStackTrace();
+			}
+
+        // Join the channel.
+		
+       bot.setVerbose(true);
+	}
+}
