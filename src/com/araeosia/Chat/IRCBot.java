f839a472f8cfa0f173e1d422bd3315d11c761869
AgentKid <agentkid20@gmail.com>
2012-09-30 14:18:18 -0400

    Replaced spaces with tabs. Work on compression.

--- src/com/araeosia/Chat/IRCBot.java
+++ src/com/araeosia/Chat/IRCBot.java
@@ -8,49 +8,49 @@ import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
 
 public class IRCBot extends PircBot{
-    protected AraeosiaChat plugin;
-    
-    public IRCBot(AraeosiaChat plugin){
-    this.plugin = plugin;
-        this.setName(plugin.username);
-    }
-    
-    @Override
-    public void onMessage(String channel, String sender, String login, String hostname, String message){
-    	if(channel.equalsIgnoreCase("#araeosia-servers")){
-    		plugin.sendToServer(plugin.decryptOrSomething(message));
-    	} else {
-    		plugin.sendToServer(message);
-    	}
-    }
-    public void startBot(){
+	protected AraeosiaChat plugin;
+	
+	public IRCBot(AraeosiaChat plugin){
+	this.plugin = plugin;
+		this.setName(plugin.username);
+	}
+	
+	@Override
+	public void onMessage(String channel, String sender, String login, String hostname, String message){
+		if(channel.equalsIgnoreCase("#araeosia-servers")){
+			plugin.sendToServer(message);
+		} else {
+			plugin.sendToServer(message);
+		}
+	}
+	public void startBot(){
    
-    // Connect to the IRC server.
-        try {
-            if(plugin.host!=null && plugin.username!=null && plugin.nick!=null){
-                this.setLogin(plugin.username);
-                this.setName(plugin.nick);
-                this.connect(plugin.host, plugin.port, plugin.password);
-            }else{
-                AraeosiaChat.log.severe("[AraeosiaChat] Cannot connect to server! Either username, host, or nick wasn't set!");
-            }
-        } catch (NickAlreadyInUseException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (IrcException e) {
-            e.printStackTrace();
-        }
-       this.setVerbose(plugin.debug);
-    }
-    
-    @Override
-    public void onConnect(){
-        if(plugin.identify){
-            this.identify(plugin.identifyPass);
-        }
-        for(String s: plugin.channels){
-     	    this.joinChannel(s);
-        }
-    }
+	// Connect to the IRC server.
+		try {
+			if(plugin.host!=null && plugin.username!=null && plugin.nick!=null){
+				this.setLogin(plugin.username);
+				this.setName(plugin.nick);
+				this.connect(plugin.host, plugin.port, plugin.password);
+			}else{
+				AraeosiaChat.log.severe("[AraeosiaChat] Cannot connect to server! Either username, host, or nick wasn't set!");
+			}
+		} catch (NickAlreadyInUseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IrcException e) {
+			e.printStackTrace();
+		}
+	   this.setVerbose(plugin.debug);
+	}
+	
+	@Override
+	public void onConnect(){
+		if(plugin.identify){
+			this.identify(plugin.identifyPass);
+		}
+		for(String s: plugin.channels){
+	 		this.joinChannel(s);
+		}
+	}
 }
